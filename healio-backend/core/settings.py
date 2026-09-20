435b4602e1f35d0b6245c0812a47c9b52b5b04e5
harshithanamala04 <harshithanamala04@gmail.com>
2026-09-20 07:47:05 -0700

    Switch Gmail SMTP to SSL port 465

--- healio-backend/core/settings.py
+++ healio-backend/core/settings.py
@@ -173,9 +173,9 @@ STATIC_URL = 'static/'
 # Gmail SMTP Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_SSL = False
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = 'harshithanamala04@gmail.com'
 EMAIL_HOST_PASSWORD = 'jgxlktrollqzqazp'
 DEFAULT_FROM_EMAIL = 'Healio Health <harshithanamala04@gmail.com>'
